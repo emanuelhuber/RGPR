693d28fca7971f68efdbbce00f74356fb72112f2
emanuelhuber <emanuel.huber@tranquille.ch>
2016-03-30 13:45:23 -0700

    add data description

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Ground-penetrating radar data.
+#'
+#' Surface ground-penetrating radar data recorded
+#' the 25 April 2014 in Frenkental (Swizerland).
+#' Coordinates: CH1903+/LV95:2'622'209.66, 1'256'907.54
+#' Elevation: 345.8 m
+#'
+#' @format An object of the class RGPR
+#'
+#' @source University of Basel (Switzerland)
+"line00"

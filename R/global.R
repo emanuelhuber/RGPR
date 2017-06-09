8f47456fec177ea7788f88147b0b182fdf42f804
emanuelhuber <emanuel.huber@tranquille.ch>
2017-06-09 23:06:50 +0200

    scaling - more robust

--- R/global.R
+++ R/global.R
@@ -1880,7 +1880,8 @@ scaleCol <- function(A, type = c("stat", "min-max", "95",
   if(!is.na(test) && test >0 && test < 100){
     A_q95 <- apply(A, 2, quantile, test/100, na.rm = TRUE)
     A_q05 <- apply(A, 2, quantile, 1 - test/100, na.rm = TRUE)
-    Ascl <- matrix(A_q95 - A_q05, nrow = nrow(A), ncol = ncol(A), byrow=TRUE)
+    Ascl <- scale(A, center = FALSE, scale = A_q95 - A_q05)
+    # matrix(A_q95 - A_q05, nrow = nrow(A), ncol = ncol(A), byrow=TRUE)
     #A <- A/Ascl
   }else{
     type <- match.arg(type)
@@ -1895,22 +1896,23 @@ scaleCol <- function(A, type = c("stat", "min-max", "95",
       # sqrt(\int  (A(t))^2 dt)
       Aamp <- matrix(apply((A)^2,2,sum), nrow = nrow(A), 
                      ncol = ncol(A), byrow=TRUE)
-      Ascl <- sqrt(Aamp)/sum(sqrt(Aamp))
+      Ascl <- A*sqrt(Aamp)/sum(sqrt(Aamp))
     }else if(type == "rms"){
       Ascl <- scale(A, center = FALSE)
       # Ascl <- matrix(apply(A ,2, .rms), nrow = nrow(A), 
       #                ncol = ncol(A), byrow=TRUE)
     }else if(type == "min-max"){  # min-max
       Ascl <- scale(A, center = FALSE, 
-                    scale = apply(A, 2, max,na.rm = TRUE) - 
-                            apply(( A),2,min,na.rm=TRUE))
+                    scale = apply(A, 2, max, na.rm = TRUE) - 
+                            apply(A, 2, min, na.rm = TRUE))
     }else if(type == "mad"){  # mad
-      Ascl <- scale(A, center = apply(z, 2, median), 
-                    scale = apply(z, 2, mad))
+      Ascl <- scale(A, center = apply(A, 2, median), 
+                    scale = apply(A, 2, mad))
     }
   }
   test <- (!is.na(Ascl[1,] ) & abs(Ascl[1,]) > .Machine$double.eps^0.75) 
   A[,test] <- Ascl[,test]
+  A[, !test] <- 0
   return(A)
 }
 

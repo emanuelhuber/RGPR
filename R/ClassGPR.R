d15a46775a05c61947704d13ab7109b9a669d858
emanuelhuber <emanuel.huber@tranquille.ch>
2018-03-25 00:35:47 +0100

    fix bugs and update documentation

--- R/ClassGPR.R
+++ R/ClassGPR.R
@@ -2954,6 +2954,26 @@ setMethod("spec", "GPR", function(x, type = c("f-x","f-k"), plotSpec = TRUE,
 #--- REMOVE DUPLICATED TRACES (TRACES WITH (ALMOST) SAME POSITIONS) ---#
 #' Remove traces with duplicated trace positions
 #' 
+#' checks for duplicates trace positions (up to precision defined by 'tol') 
+#' and remove them from 'x' (object of the class GPR or GPRsurvey). 
+#' If there is a series of consecutive traces with an inter-distance smaller
+#' than the tolerance threshold defined by the computer precision, 
+#' the function starts by removing traces every two traces and repeat 
+#' the procedure until the trace inter-distances are larger that the threshold.
+#' Example with 5 traces:
+#' \itemize{
+#'   \item distance between trace 1 and 2 < tol
+#'   \item distance between trace 2 and 3 < tol
+#'   \item distance between trace 3 and 4 < tol
+#'   \item distance between trace 4 and 5 < tol
+#' }
+#' The algorithm first remove trace 2 and 4 and recompute 
+#' the inter-trace distances:
+#' \itemize{
+#'   \item distance between trace 1 and 3 < tol
+#'   \item distance between trace 3 and 5 > tol
+#' }  
+#' The algorithm remove trace 3. END!
 #' @param x   An object of the class GPR
 #' @param tol Length-one numeric vector: if the horizontal distance between two 
 #'            consecutive traces is smaller than \code{tol}, then

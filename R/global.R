299dc7a63820b3537f399c577054b66c7a3ca1d4
emanuelhuber <emanuel.huber@unibas.ch>
2019-05-24 16:15:50 +0200

    add new function 'closestTr()' to project any spatial object on the GPR data

--- R/global.R
+++ R/global.R
@@ -1280,6 +1280,20 @@ posLine <- function(loc,last=FALSE){
   }
 }
   
+#' Closest trace
+#' 
+#' Return the indice of the closest trace to the point \code{y}
+#' @param x Object of the class GPR.
+#' @param y Length-two numeric vector of the (x, y)-coordinates.
+#' @return Indice (integer) of the closest trace.
+#' @export
+closestTr <- function(x, y){
+  ymat <- matrix(as.numeric(y[1:2]), 
+                 nrow = length(x),
+                 ncol = 2,
+                 byrow = TRUE)
+  which.min(rowSums((ymat - x@coord[,1:2])^2))
+}
 
 
 #' latitude-longitude to UTM

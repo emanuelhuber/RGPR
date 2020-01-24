183522830d3f572e5ffc7c1f9c4e086bf625c1c3
emanuelhuber <emanuel.huber@tranquille.ch>
2020-01-24 10:16:09 +0100

    add function to interpolate raster from matrix

--- R/spInterpToRaster.R
+++ R/spInterpToRaster.R
@@ -34,3 +34,34 @@ setMethod("spInterpToRaster", "GPRsurvey", function(x, nx = 100, ny = 100, h = 5
   r <- raster::mask(r, spsb)
   return(r)
 })
+
+#' @export
+setMethod("spInterpToRaster", "matrix", function(x, nx = 100, ny = 100, h = 5, CRSobj, ...){
+  # better way to do
+  # xyz <- do.call(rbind, coords(x))
+  xyz <- x
+  
+  rr <- as.vector(apply(xyz[, 1:2], 2, range, na.rm = TRUE))
+  D <- 0.05 * (rr[c(2, 4)] - rr[c(1, 3)])
+  
+  test <- chull(xyz[,1], xyz[, 2])
+  
+  p = sp::Polygon(xyz[test, 1:2])
+  ps = sp::Polygons(list(p),1)
+  sps = sp::SpatialPolygons(list(ps))
+  
+  spsb <- raster::buffer(sps, width = min(D))
+  
+  # raster::plot(spsb, add = TRUE)
+  
+  # rough interpolation (h = 10)
+  S <- MBA::mba.surf(xyz, no.X = nx, no.Y = ny, h = h, extend = TRUE,
+                     b.box = rr + c(-D[1], D[1], -D[2], D[2]))
+  
+  r <- raster::raster(S$xyz.est)
+  raster::crs(r) <- CRSobj
+  
+  r <- raster::mask(r, spsb)
+  return(r)
+})
+

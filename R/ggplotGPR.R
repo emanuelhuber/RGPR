4bb81503f00d1dcf5cfef1771ad91eb4355f3846
emanuelhuber <emanuel.huber@tranquille.ch>
2021-11-19 09:23:37 +0100

    ggplot for GPR data - fix bug

--- R/ggplotGPR.R
+++ R/ggplotGPR.R
@@ -11,7 +11,7 @@ ggplotGPR <- function(x, col = palGPR()){
                    y = rep(depth(x), each = ncol(x)),
                    value =  as.vector(t(as.matrix(x)[nrow(x):1, ])))
   ggplot2::ggplot(Xm) +
-    ggplot2::geom_tile(aes(x = x, y = y, fill = value)) +
+    ggplot2::geom_tile(ggplot2::aes(x = x, y = y, fill = value)) +
     ggplot2::scale_x_continuous("X", expand = c(0, 0)) +
     ggplot2::scale_y_continuous("Y", expand = c(0, 0)) +
     ggplot2::scale_fill_gradientn("Z", colours = col) 

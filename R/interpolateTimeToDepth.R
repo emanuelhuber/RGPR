0cfb5159321f5e0c7f77fb1f3b8fc6b843ad0685
emanuelhuber <emanuel.huber@tranquille.ch>
2022-09-18 22:48:32 +0200

    interpolat time to depth based on 'borehole' information

--- R/interpolateTimeToDepth.R
+++ R/interpolateTimeToDepth.R
@@ -14,20 +14,20 @@
 # 2D interpolation (2 Bohrungen)
 # 
 # interpolateTimeToDepth(x,
-#                        
+# #                        
 #                        z = list(c(1.4,  2,  3.5),
-#                                 
+# 
 #                                 c(  2, 2.7, 4 )) ,
-#                        
+# 
 #                        twt = list(c(45, 180, 230),
-#                                   
+# 
 #                                   c(34, 89, 120)),
-#                        
+# 
 #                        pos = c(5.2, 23.5))
+# 
+# x <- frenkeLine00
 
-
-
-interpolateTimeToDepth <- function(x, z = NULL , twt = NULL, pos = NULL, method = "linear"){
+interpolateTimeToDepth <- function(x, z = NULL , twt = NULL, pos = NULL, method = "linear", h = 2){
   if(length(z) != length(twt)){
     stop("'z' and 'twt' must have the same length!")
   }
@@ -37,6 +37,14 @@ interpolateTimeToDepth <- function(x, z = NULL , twt = NULL, pos = NULL, method 
   }
   if(is.list(z) && is.list(twt)){
     if(!is.null(pos) && length(pos) == length(z)){
+      D <- list()
+      for(i in seq_along(pos)){
+        D[[i]] <- signal::interp1(x = twt[[i]],
+                                  y = z[[i]],
+                                  xi = x@depth,
+                                  method = method,
+                                  extrap = TRUE)
+      }
       dx <- mean(diff(pos(x)))
       dy <- mean(diff(depth(x)))
       
@@ -57,26 +65,38 @@ interpolateTimeToDepth <- function(x, z = NULL , twt = NULL, pos = NULL, method 
       if(m < 1) m <- 1L
       if(n < 1) n <- 1L
       
-      zz <- unlist(z)
-      twttwt <- unlist(twt)
-      pospos <- rep(pos,  sapply(z, length))
-      D00 <- MBA::mba.surf(cbind(pospos, twttwt, zz), no.X = para$nx , no.Y = para$ny, n = n, m = m, 
-                          extend = TRUE, h = 6, b.box = para$bbox)$xyz.est
-      D0 <- D00$z
-      plot3D::image2D()
+      dd <- c(unlist(D), rep(0, length(pos(x))))
+      twttwt <- c(rep(depth(x),  length(D)), rep(0, length(pos(x))))
+      pospos <- c(rep(pos,  sapply(D, length)), pos(x))
+      # plot3D::points2D(pospos, twttwt, colvar = dd)
+      DEP <- verboseF( MBA::mba.surf(cbind(pospos, twttwt, dd), no.X = para$nx , no.Y = para$ny, n = n, m = m, 
+                          extend = TRUE, h = h, b.box = para$bbox), verbose = FALSE)
+      DD <- matrix(NA, ncol = length(depth(x)), nrow = length(pos(x)))
+      DD[] <- as.vector(DEP$xyz.est$z)
+      # plot3D::image2D(DD, DEP$xyz.est$x, DEP$xyz.est$y)
+      # 
+      # x1@data[] <- t(DD)
+      # plot(x1)
+      
+      D0 <- t(DD)
+      
       if(all(dim(D0) == dim(x))){
         dz <- mean(D0[2:nrow(D0),] - D0[1:(nrow(D0)-1), ], na.rm = TRUE)
         zv <- seq(from = min(D0, na.rm = TRUE), to = max(D0, na.rm = TRUE), by = dz)
+        xnew <- x
+        xnew@data <- x@data[rep(1, length(zv)),]
         for(i in 1:ncol(x)){
-          x@data[, i] <- signal::interp1(x = D0[, i],
+          xnew@data[, i] <- signal::interp1(x = D0[, i],
                                          y = x@data[, i],
                                          xi = zv,
                                          method = method,
-                                         extrap = TRUE)
+                                         extrap = FALSE)
         }
-        x@depth <- zv
-        x@depthunit <- x@posunit
-        x@dz        <- mean(diff(x@depth), na.rm = TRUE)
+        xnew@depth <- zv
+        xnew@depthunit <- xnew@posunit
+        xnew@dz        <- mean(diff(xnew@depth), na.rm = TRUE)
+        return(xnew)
+        # plot(xnew)
       }else{
         stop("Problem")
       }
